a572ccf2a3ac0a6e8138886ce2c4be0137225568
profil <bullenskex@gmail.com>
2012-06-28 11:51:37 +0200

    Create directories needed in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -e
 
+mkdir bin src cross system
 export PATH=$(pwd)/cross/bin:$(pwd)/bin:$PATH
 
 # Set up cross-compiling
 # build musl
-build pkg/cross/binutils*
+#build pkg/cross/binutils*
 #build pkg/cross/gcc*
-build pkg/cross/musl*
+#build pkg/cross/musl*
 
